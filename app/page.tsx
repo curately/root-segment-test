394fc66ca8d719196d53d4532fdcaba67a964a3b
Chris Forte <80777374+indie-ridge-dev@users.noreply.github.com>
2024-06-13 09:39:34 +0100

    update links on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,17 @@
 /** Add your relevant code here for the issue to reproduce */
+import Link from "next/link";
 export default function Home() {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <h1>Homepage</h1>
+      <ul>
+        <li>
+          <Link href="curation/waterpark">Valid Link</Link>
+        </li>
+        <li>
+          <Link href="curation/i-dont-exist">This link should 404</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
