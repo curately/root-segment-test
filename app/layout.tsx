ebd6517ec5f19cc176d8407b2739c2582d41e87e
Chris Forte <80777374+indie-ridge-dev@users.noreply.github.com>
2024-06-14 14:40:36 +0100

    add strict mode in page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
+import { StrictMode, Suspense } from "react";
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <StrictMode>{children}</StrictMode>
+      </body>
     </html>
   );
 }
